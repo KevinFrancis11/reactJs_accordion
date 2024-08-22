b314710dc90399146fd191c740784487b5312eaf
Kevin Francis <kevinfrancis967@gmail.com>
2024-08-22 20:46:23 +0530

    last commit before going to above medium level

--- src/components/Accordions.jsx
+++ src/components/Accordions.jsx
@@ -4,13 +4,17 @@ import { useState } from "react";
 
 const Accordions = ({ accordionData }) => {
   //component states
-  const [accordionIds, setAccordionIds] = useState([])
+  const [accordionIds, setAccordionIds] = useState([]);
 
 
   //handler functions 
-  const handlerAccordion = (id) => {
-    setAccordionIds([id]);
-  }
+  const handlerAccordion = (newId) => {
+    setAccordionIds((prevAccID) => {
+      return accordionIds.includes(newId) ? accordionIds.filter((acc_id) => acc_id !== newId) : [...prevAccID, newId];
+    });//the filter method creates a new array and return that array.
+       //the includes method checks if the newId is already exist in the accordionIds array.
+  }   //this logic is helpfull in making cart logic in ecommerce projects , for ex:- we dont want to 
+      //again and again add the same pair of jeans into the cart we only need to increment the quantity of the jeans. 
 
   console.log(accordionIds);
 
